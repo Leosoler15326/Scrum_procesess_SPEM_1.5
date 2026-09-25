1cd343cd08430dccb2e5bd5c2507f44b43a4b43a
Leosoler <leitosolerleon@gmail.com>
2026-09-25 18:13:50 -0500

    Initial commit

---
+++ scripts/processElementData.js
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_SJ7MQbkeEfGjjdEHoxBqNQActivityDetail"]="./../../Scrum/capabilitypatterns/resources/Planning_A0646773_a0646773_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_WyAxsbkeEfGjjdEHoxBqNQActivityDetail"]="./../../Scrum/capabilitypatterns/resources/Execution_BDE137DD_bde137dd_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_YsNNgbkeEfGjjdEHoxBqNQActivityDetail"]="./../../Scrum/capabilitypatterns/resources/Review_4DC42BCA_4dc42bca_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_anQk8bkeEfGjjdEHoxBqNQActivityDetail"]="./../../Scrum/capabilitypatterns/resources/Retrospective_2F5F4786_2f5f4786_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_dbQhIbkfEfGjjdEHoxBqNQActivityDetail"]="./../../Scrum/deliveryprocesses/resources/Sprint Iteration_2A513E64_2a513e64_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["__uqVY7keEfGjjdEHoxBqNQ"] = ["Product Backlog","Scrum/capabilitypatterns/product_backlog_5A092BAE.html"];
+contentPage.processPage.elementUrls["_jt0rYLkfEfGjjdEHoxBqNQ"] = ["Create and prioritize the Product Backlog","Scrum/deliveryprocesses/create_and_prioritize_the_product_backlog_BB851E55.html"];
+contentPage.processPage.elementUrls["_6jWuIrkeEfGjjdEHoxBqNQ"] = ["Sprint Goals","Scrum/capabilitypatterns/sprint_goals_F345D4B0.html"];
+contentPage.processPage.elementUrls["_QPQZ47kfEfGjjdEHoxBqNQ"] = ["Developer","Scrum/capabilitypatterns/developer_5FD1F241.html"];
+contentPage.processPage.elementUrls["_WyAxsbkeEfGjjdEHoxBqNQ"] = ["Execution","Scrum/capabilitypatterns/Execution_BDE137DD.html"];
+contentPage.processPage.elementUrls["_QPQZ57kfEfGjjdEHoxBqNQ"] = ["Sprint Backlog","Scrum/capabilitypatterns/sprint_backlog_2463D460.html"];
+contentPage.processPage.elementUrls["_l2ZhcbkfEfGjjdEHoxBqNQ"] = ["Software Increment","Scrum/deliveryprocesses/software_increment_1D528BA9.html"];
+contentPage.processPage.elementUrls["__uqVYbkeEfGjjdEHoxBqNQ"] = ["Product Owner","Scrum/capabilitypatterns/product_owner_E69175D9.html"];
+contentPage.processPage.elementUrls["_6MFdoLkeEfGjjdEHoxBqNQ"] = ["Develop the increment","Scrum/capabilitypatterns/develop_the_increment_B052E767.html"];
+contentPage.processPage.elementUrls["_l1tk8bkfEfGjjdEHoxBqNQ"] = ["Developer","Scrum/deliveryprocesses/developer_FBA785DC.html"];
+contentPage.processPage.elementUrls["_QPQZ4LkfEfGjjdEHoxBqNQ"] = ["Conduct the Retrospective","Scrum/capabilitypatterns/conduct_the_retrospective_FDC12856.html"];
+contentPage.processPage.elementUrls["_rmyoMrkeEfGjjdEHoxBqNQ"] = ["Product Backlog","Scrum/capabilitypatterns/product_backlog_A430290B.html"];
+contentPage.processPage.elementUrls["_L0Cy1LkfEfGjjdEHoxBqNQ"] = ["Sprint Goals","Scrum/capabilitypatterns/sprint_goals_B91849EB.html"];
+contentPage.processPage.elementUrls["_QPQZ4bkfEfGjjdEHoxBqNQ"] = ["Scrum Master","Scrum/capabilitypatterns/scrum_master_EC5A3C6C.html"];
+contentPage.processPage.elementUrls["_l1tk9bkfEfGjjdEHoxBqNQ"] = ["Sprint Backlog","Scrum/deliveryprocesses/sprint_backlog_C03967FB.html"];
+contentPage.processPage.elementUrls["_anQk8bkeEfGjjdEHoxBqNQ"] = ["Retrospective","Scrum/capabilitypatterns/Retrospective_2F5F4786.html"];
+contentPage.processPage.elementUrls["_l1tk8LkfEfGjjdEHoxBqNQ"] = ["Plan the Sprint","Scrum/deliveryprocesses/plan_the_sprint_D0E71C6.html"];
+contentPage.processPage.elementUrls["_6MFdobkeEfGjjdEHoxBqNQ"] = ["Developer","Scrum/capabilitypatterns/developer_9EEBFB7D.html"];
+contentPage.processPage.elementUrls["_6jTDwLkeEfGjjdEHoxBqNQ"] = ["Hold the Daily Scrum","Scrum/capabilitypatterns/hold_the_daily_scrum_37C3490E.html"];
+contentPage.processPage.elementUrls["_YsNNgbkeEfGjjdEHoxBqNQ"] = ["Review","Scrum/capabilitypatterns/Review_4DC42BCA.html"];
+contentPage.processPage.elementUrls["_oSyEULkfEfGjjdEHoxBqNQ"] = ["Conduct the Retrospective","Scrum/deliveryprocesses/conduct_the_retrospective_9055877D.html"];
+contentPage.processPage.elementUrls["_rmyoMLkeEfGjjdEHoxBqNQ"] = ["Product Owner","Scrum/capabilitypatterns/product_owner_AAF934E5.html"];
+contentPage.processPage.elementUrls["_QPQZ5bkfEfGjjdEHoxBqNQ"] = ["Software Increment","Scrum/capabilitypatterns/software_increment_B0EC1E8B.html"];
+contentPage.processPage.elementUrls["__vJdkrkeEfGjjdEHoxBqNQ"] = ["Done Definition","Scrum/capabilitypatterns/done_definition_C5B3E1AD.html"];
+contentPage.processPage.elementUrls["_jt0rYbkfEfGjjdEHoxBqNQ"] = ["Product Owner","Scrum/deliveryprocesses/product_owner_AA1E326B.html"];
+contentPage.processPage.elementUrls["_6jTDwbkeEfGjjdEHoxBqNQ"] = ["Scrum Master","Scrum/capabilitypatterns/scrum_master_265C5D24.html"];
+contentPage.processPage.elementUrls["_6MFdo7keEfGjjdEHoxBqNQ"] = ["Sprint Backlog","Scrum/capabilitypatterns/sprint_backlog_1263B152.html"];
+contentPage.processPage.elementUrls["_L0CLwLkfEfGjjdEHoxBqNQ"] = ["Review the Sprint","Scrum/capabilitypatterns/review_the_sprint_5208EF78.html"];
+contentPage.processPage.elementUrls["_SJ7MQbkeEfGjjdEHoxBqNQ"] = ["Planning","Scrum/capabilitypatterns/Planning_A0646773.html"];
+contentPage.processPage.elementUrls["_6MFdpbkeEfGjjdEHoxBqNQ"] = ["Software Increment","Scrum/capabilitypatterns/software_increment_637DDD9C.html"];
+contentPage.processPage.elementUrls["_rmxaELkeEfGjjdEHoxBqNQ"] = ["Create and prioritize the Product Backlog","Scrum/capabilitypatterns/create_and_prioritize_the_product_backlog_5F76F500.html"];
+contentPage.processPage.elementUrls["_nFZrMLkfEfGjjdEHoxBqNQ"] = ["Hold the Daily Scrum","Scrum/deliveryprocesses/hold_the_daily_scrum_5BD67DC5.html"];
+contentPage.processPage.elementUrls["_jt0rY7kfEfGjjdEHoxBqNQ"] = ["Product Backlog","Scrum/deliveryprocesses/product_backlog_1D95E840.html"];
+contentPage.processPage.elementUrls["_AEW4kLkfEfGjjdEHoxBqNQ"] = ["Plan the Sprint","Scrum/capabilitypatterns/plan_the_sprint_2CAB0A18.html"];
+contentPage.processPage.elementUrls["_L0Cy1rkfEfGjjdEHoxBqNQ"] = ["Product Backlog","Scrum/capabilitypatterns/product_backlog_B24F3E11.html"];
+contentPage.processPage.elementUrls["_jt15gbkfEfGjjdEHoxBqNQ"] = ["Done Definition","Scrum/deliveryprocesses/done_definition_50211B7F.html"];
+contentPage.processPage.elementUrls["_dbQhIbkfEfGjjdEHoxBqNQ"] = ["Sprint Iteration","Scrum/deliveryprocesses/Sprint Iteration_2A513E64.html"];
+contentPage.processPage.elementUrls["_n-77QLkfEfGjjdEHoxBqNQ"] = ["Review the Sprint","Scrum/deliveryprocesses/review_the_sprint_21C93630.html"];
+contentPage.processPage.elementUrls["_L0Cy0rkfEfGjjdEHoxBqNQ"] = ["Software Increment","Scrum/capabilitypatterns/software_increment_EDBD5BF2.html"];
+contentPage.processPage.elementUrls["_mhuWYLkfEfGjjdEHoxBqNQ"] = ["Develop the increment","Scrum/deliveryprocesses/develop_the_increment_75914466.html"];
+contentPage.processPage.elementUrls["_L0Cy0LkfEfGjjdEHoxBqNQ"] = ["Product Owner","Scrum/capabilitypatterns/product_owner_F48667CC.html"];
+contentPage.processPage.elementUrls["_l1tk87kfEfGjjdEHoxBqNQ"] = ["Scrum Master","Scrum/deliveryprocesses/scrum_master_6F1F3BB1.html"];
+contentPage.processPage.elementUrls["_l1tk97kfEfGjjdEHoxBqNQ"] = ["Sprint Goals","Scrum/deliveryprocesses/sprint_goals_33B11DD0.html"];
